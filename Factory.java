9214b7f91fe407945514cf1b1cc40df601b37a82
Pepitorino <carlos.ambrosio2007@gmail.com>
2023-06-25 17:04:25 +0800

    added some things for items

--- Factory.java
+++ Factory.java
@@ -9,20 +9,28 @@ public class Factory {
         int x = -1;
         Scanner input = new Scanner(System.in);
 
+        NewRegVendMachine newVM = new NewRegVendMachine();
+
         do {
             try {
-                System.out.printf("\nHow many items will this vending machine have? (Max 8)\nINPUT:");
+                System.out.printf("\nHow many items will this vending machine have? (Max 16)\nINPUT:");
                 x = input.nextInt();       
             }
             catch (InputMismatchException e) {
                 System.out.printf("\nINVALID INPUT\n");
                 input.nextLine();
             }
-        } while ( x<1 || x>8 );
+        } while ( x<1 || x>16 );
 
         for (int i = 0 ; i<x ; i++) {
             System.out.printf("\nItem #%d\n", i+1);
+            System.out.printf("Enter item name: ");
+            do {
+                
+            } while (input.nextLine().equals("\n"));
         }
+
+        this.vm = newVM;
 
     }
 
