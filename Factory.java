f47490d4ea645b679eae1f9b808fd7c5dd282134
Pepitorino <carlos.ambrosio2007@gmail.com>
2023-06-25 17:17:31 +0800

    put item input in it's own method

--- Factory.java
+++ Factory.java
@@ -5,6 +5,52 @@ import java.util.InputMismatchException;
 public class Factory {
     private NewRegVendMachine vm;
 
+    public void inputItem(NewRegVendMachine vm, int itemNum) {
+        Scanner input = new Scanner();
+        for (int i = 0 ; i<itemNum ; i++) {
+            String tempName = "\n";
+            double tempPrice = 0;
+            double tempCal = 0;
+            System.out.printf("\nItem #%d\n", i+1);
+            System.out.printf("Enter item name: ");
+
+            do {
+                tempName = input.nextLine();
+                if (tempName.equals("\n")) System.out.printf("\nINVALID INPUT\n");
+            } while (tempName.equals("\n"));
+
+            input.nextLine();
+
+            do {
+                try {
+                    System.out.printf("Enter item price: ");
+                    tempPrice = input.nextDouble();       
+                }
+                catch (InputMismatchException e) {
+                    System.out.printf("\nINVALID INPUT\n");
+                    input.nextLine();
+                }
+                if (tempPrice<0) System.out.printf("\nPRICE CANNOT BE NEGATIVE\n");
+            } while (tempPrice<0);
+            
+            input.nextLine();
+
+            do {
+                try {
+                    System.out.printf("Enter item calories: ");
+                    tempCal = input.nextDouble();       
+                }
+                catch (InputMismatchException e) {
+                    System.out.printf("\nINVALID INPUT\n");
+                    input.nextLine();
+                }
+                if (tempCal<0) System.out.printf("\nCALORIES CANNOT BE NEGATIVE\n");
+            } while (tempCal<0);
+
+            input.nextLine();
+        }
+    }
+
     public void createVM() {
         int x = -1;
         Scanner input = new Scanner(System.in);
@@ -22,13 +68,9 @@ public class Factory {
             }
         } while ( x<1 || x>16 );
 
-        for (int i = 0 ; i<x ; i++) {
-            System.out.printf("\nItem #%d\n", i+1);
-            System.out.printf("Enter item name: ");
-            do {
-                
-            } while (input.nextLine().equals("\n"));
-        }
+        input.nextLine();
+
+        inputItem(newVM, x);
 
         this.vm = newVM;
 
