99bbafe40590b0c225e5fcd8904cd63d716d6a64
Pepitorino <carlos.ambrosio2007@gmail.com>
2023-06-25 16:51:33 +0800

    added input validation for vending machine items

--- Factory.java
+++ Factory.java
@@ -1,5 +1,6 @@
 import VendingMachine.*;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Factory {
     private NewRegVendMachine vm;
@@ -9,12 +10,18 @@ public class Factory {
         Scanner input = new Scanner(System.in);
 
         do {
-            System.out.printf("\nHow many items will this vending machine have? (Max 8)\nINPUT:");
-            x = input.nextInt();   
+            try {
+                System.out.printf("\nHow many items will this vending machine have? (Max 8)\nINPUT:");
+                x = input.nextInt();       
+            }
+            catch (InputMismatchException e) {
+                System.out.printf("\nINVALID INPUT\n");
+                input.nextLine();
+            }
         } while ( x<1 || x>8 );
 
         for (int i = 0 ; i<x ; i++) {
-            System.out.printf("\nItem #%d\n", i);
+            System.out.printf("\nItem #%d\n", i+1);
         }
 
     }
