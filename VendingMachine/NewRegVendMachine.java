23b5f3f0a1028d5a5b4a3e822c1c73752db03e71
Pepitorino <carlos.ambrosio2007@gmail.com>
2023-06-25 16:41:37 +0800

    fixed formatting

--- VendingMachine/NewRegVendMachine.java
+++ VendingMachine/NewRegVendMachine.java
@@ -5,22 +5,46 @@ import java.util.ArrayList;
 public class NewRegVendMachine {
     private class itemStack {
         private ArrayList<Item> items;
-        Item itemType;
+        private Item itemType;
 
-        public int getNumOfItems() {
-            return items.length(); 
+        public int getNumItems() {
+            return this.items.length(); 
         }
 
         public Item popItem() {
-            Item temp = items.get(items.length()-1);
-            items.remove(temp);
+            Item temp = this.items.get((this.items.length())-1);
+            this.items.remove(temp);
             return temp;
         }
 
         public void pushItem() {
-            items.add(new Item(itemType));
+            this.items.add(new Item(this.itemType));
+        }
+
+        public String getItemName() {
+            return this.itemType.getName();
+        }
+
+        public double getItemPrice() {
+            return this.itemType.getPrice();
+        }
+
+        public double getItemCalories() {
+            return this.itemType.getCalories();
         }
     }
-    ArrayList<itemStack> itemsAvailable;
+
+    ArrayList<itemStack> itemTypes;
+
+    public NewRegVendMachine() {
+
+    }
+
+    //Testing Features
+
+    //Maintenance Features
+    public void restockItem() {
+        
+    }
 }
 
