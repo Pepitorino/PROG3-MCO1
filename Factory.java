04b27139da99a467360ae700b8dfbf15da077b88
Pepitorino <carlos.ambrosio2007@gmail.com>
2023-06-25 16:42:02 +0800

    added basic functionality and only uses one vm now

--- Factory.java
+++ Factory.java
@@ -1,9 +1,24 @@
+import VendingMachine.*;
+import java.util.Scanner;
+
 public class Factory {
-    ArrayList<RegVendMachine> vendingMachines;
+    private NewRegVendMachine vm;
 
     public void createVM() {
+        int x = -1;
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.printf("\nHow many items will this vending machine have? (Max 8)\nINPUT:");
+            x = input.nextInt();   
+        } while ( x<1 || x>8 );
+
+        for (int i = 0 ; i<x ; i++) {
+            System.out.printf("\nItem #%d\n", i);
+        }
 
     }
+
     public void testVM() {
 
     }
